63f72f3c88290c8bb1f964311818c5176fb76b22
Scar <scarosaurus@gmail.com>
2023-01-25 18:07:38 -0500

    Hash + Salting

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -21,8 +21,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 })
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]})
 
 const User = new mongoose.model("User", userSchema)
 
@@ -39,18 +37,22 @@ app.get("/register", function (req, res) {
 })
 
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  })
-
-  newUser.save(function (err) {
-    if (err) {
-      console.log(err)
-    } else {
-      res.render("secrets")
-    }
-  })
+  bcrypt.genSalt(saltRounds, function(err, salt) {
+    bcrypt.hash(req.body.password, salt, function(err, hash) {
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+      })
+    
+      newUser.save(function (err) {
+        if (err) {
+          console.log(err)
+        } else {
+          res.render("secrets")
+        }
+      })
+})
+})
 })
 
 app.post("/login", function (req, res) {
@@ -62,9 +64,11 @@ app.post("/login", function (req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets")          
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets")          
+          }
+      });
       }
     }
   })
